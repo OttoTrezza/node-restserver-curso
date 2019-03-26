a10e64752425307ddee6a08380ea3e6e84512b34
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2019-03-26 10:54:57 -0300

    URL_DB_remota(OCULTA)

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
 
-    urlDB = 'mongodb+srv://Marsupion:3noqHdazT3Qk94T9@cluster0-9xz8q.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
